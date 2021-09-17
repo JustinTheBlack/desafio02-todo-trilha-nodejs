d63a1b504f7b5d195e0814c4c3f100c794efe426
Justino José <justinojosesilva@gmail.com>
2021-09-17 14:25:32 -0300

    Desafio 2 concluido

--- src/index.js
+++ src/index.js
@@ -10,19 +10,51 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const userExists = users.find((user) => user.username === username);
+  if(!userExists) {
+    return response.status(404).json({ error: "Username not found!" });
+  }
+  request.user = userExists
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  const exceededlimit = user.todos.length >= 9;
+  if ((!user.pro && !exceededlimit) || (user.pro)) {
+    return next();
+  }
+  return response.status(403).json({ error: "Limit Exceeded Please Turn Pro!" });
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  const userExists = users.find((user) => user.username === username);
+  if(!userExists) {
+    return response.status(404).json({ error: "Username not found!" });
+  }
+  if(!validate(id)) {
+    return response.status(400).json({ error: "Id is not valid!" });
+  }
+  const todoExist = userExists.todos.find((todo) => todo.id === id);
+  if(!todoExist) {
+    return response.status(404).json({ error: "Todo not found!" });
+  }
+  request.user = userExists;
+  request.todo = todoExist;
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const userExists = users.find((user) => user.id === id);
+  if(!userExists) {
+    return response.status(404).json({ error: "User not found!" });
+  }
+  request.user = userExists;
+  return next();
 }
 
 app.post('/users', (request, response) => {
